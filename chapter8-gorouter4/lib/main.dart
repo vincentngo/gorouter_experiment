c0cf59171255809dcd5b2b4df82ea9ffc0b5039a
Vincent Ngo <vincentvyngo@yahoo.com>
2022-07-20 16:17:15 +0700

    more updates

--- chapter8-gorouter4/lib/main.dart
+++ chapter8-gorouter4/lib/main.dart
@@ -5,18 +5,20 @@ import 'fooderlich_theme.dart';
 import 'models/models.dart';
 import 'navigation/app_router.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final appStateManager = AppStateManager();
-  appStateManager.initializeApp();
-  runApp(Fooderlich(
-    appStateManager: appStateManager));
+  await appStateManager.initializeApp();
+  runApp(Fooderlich(appStateManager: appStateManager));
 }
 
 class Fooderlich extends StatefulWidget {
   final AppStateManager appStateManager;
 
-  const Fooderlich({super.key, required this.appStateManager,});
+  const Fooderlich({
+    super.key,
+    required this.appStateManager,
+  });
 
   @override
   _FooderlichState createState() => _FooderlichState();
@@ -25,25 +27,15 @@ class Fooderlich extends StatefulWidget {
 class _FooderlichState extends State<Fooderlich> {
   late final _groceryManager = GroceryManager();
   late final _profileManager = ProfileManager();
-
+  late final _appRouter =
+      AppRouter(widget.appStateManager, _profileManager, _groceryManager);
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => _groceryManager),
-        ChangeNotifierProvider(
-          create: (context) => widget.appStateManager,
-        ),
-        ChangeNotifierProvider(
-          create: (context) => _profileManager,
-        ),
-        Provider<AppRouter>(
-          lazy: false,
-          create: (BuildContext createContext) => AppRouter(
-            widget.appStateManager,
-            _profileManager, 
-            _groceryManager)
-        ),
+        ChangeNotifierProvider(create: (context) => widget.appStateManager),
+        ChangeNotifierProvider(create: (context) => _profileManager)
       ],
       child: Consumer<ProfileManager>(
         builder: (context, profileManager, child) {
@@ -54,13 +46,12 @@ class _FooderlichState extends State<Fooderlich> {
             theme = FooderlichTheme.light();
           }
 
-          final router = Provider.of<AppRouter>(context, listen: false).router;
+          final router = _appRouter.router;
 
           return MaterialApp.router(
             debugShowCheckedModeBanner: false,
             theme: theme,
             title: 'Fooderlich',
-            // backButtonDispatcher: RootBackButtonDispatcher(),
             routeInformationParser: router.routeInformationParser,
             routeInformationProvider: router.routeInformationProvider,
             routerDelegate: router.routerDelegate,
